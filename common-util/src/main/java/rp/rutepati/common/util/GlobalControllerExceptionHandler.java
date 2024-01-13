9b731df060a01d7b1606853d56dfb12741942dc3
Rutesh Patil <ruteshp21@gmail.com>
2024-01-13 22:14:26 +0530

    #3 Adding RestControllerAdvice for exception handling

---
+++ common-util/src/main/java/rp/rutepati/common/util/GlobalControllerExceptionHandler.java
@@ -0,0 +1,43 @@
+package rp.rutepati.common.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import rp.rutepati.common.api.exceptions.InvalidInputException;
+import rp.rutepati.common.api.exceptions.NotFoundException;
+
+import javax.management.InstanceNotFoundException;
+
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public @ResponseBody HttpErrorInfo handleNotFoundException(ServerHttpRequest request, NotFoundException ex) {
+        return createHttpErrorInfo(HttpStatus.NOT_FOUND, request, ex);
+    }
+
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(InvalidInputException.class)
+    public @ResponseBody HttpErrorInfo handleInvalidInputException(ServerHttpRequest request, InvalidInputException ex) {
+        return createHttpErrorInfo(HttpStatus.UNPROCESSABLE_ENTITY, request, ex);
+    }
+
+    private HttpErrorInfo createHttpErrorInfo(
+            HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
+
+        final String path = request.getPath().pathWithinApplication().value();
+        final String message = ex.getMessage();
+
+        LOG.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
+        return new HttpErrorInfo(httpStatus, path, message);
+    }
+
+}
