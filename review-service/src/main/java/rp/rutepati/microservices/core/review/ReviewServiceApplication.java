4311a3b7f5551d1f313c9f9c9f8621165c50251b
Rutesh Patil <ruteshp21@gmail.com>
2024-01-14 11:02:10 +0530

    #5 Adding implementation for ReviewApi

--- review-service/src/main/java/rp/rutepati/microservices/core/review/ReviewServiceApplication.java
+++ review-service/src/main/java/rp/rutepati/microservices/core/review/ReviewServiceApplication.java
@@ -2,12 +2,14 @@ package rp.rutepati.microservices.core.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = {"rp.rutepati.microservices", "rp.rutepati.common"})
 public class ReviewServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReviewServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewServiceApplication.class, args);
+    }
 
 }
