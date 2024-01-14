c987b83aff8eea5da50bdd8acc6aaff394c0f98c
Rutesh Patil <ruteshp21@gmail.com>
2024-01-14 11:07:37 +0530

    #6 Adding implementation for RecommendationApi

--- recommendation-service/src/main/java/rp/rutepati/microservices/core/recommendation/RecommendationServiceApplication.java
+++ recommendation-service/src/main/java/rp/rutepati/microservices/core/recommendation/RecommendationServiceApplication.java
@@ -2,12 +2,14 @@ package rp.rutepati.microservices.core.recommendation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = {"rp.rutepati.microservices", "rp.rutepati.common"})
 public class RecommendationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecommendationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecommendationServiceApplication.class, args);
+    }
 
 }
