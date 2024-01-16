8057f031d3da07731265db90e77f4c13d63b735d
Rutesh Patil <ruteshp21@gmail.com>
2024-01-16 22:55:26 +0530

    #12 Adding OpenApi support for product-aggregate-service

--- common-api/src/main/java/rp/rutepati/common/api/aggregate/product/ProductAggregateApi.java
+++ common-api/src/main/java/rp/rutepati/common/api/aggregate/product/ProductAggregateApi.java
@@ -1,10 +1,24 @@
 package rp.rutepati.common.api.aggregate.product;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Tag(name = "ProductAggregate", description = "REST API for aggregate product information.")
 public interface ProductAggregateApi {
 
+    @Operation(
+            summary = "${api.product-composite.get-composite-product.description}",
+            description = "${api.product-composite.get-composite-product.notes}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.responseCodes.ok.description}"),
+            @ApiResponse(responseCode = "400", description = "${api.responseCodes.badRequest.description}"),
+            @ApiResponse(responseCode = "404", description = "${api.responseCodes.notFound.description}"),
+            @ApiResponse(responseCode = "422", description = "${api.responseCodes.unprocessableEntity.description}")
+    })
     @GetMapping(
             value = "/product-aggregate/{productId}",
             produces = "application/json")
